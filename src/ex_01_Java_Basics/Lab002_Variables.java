bdea2ebcc6cfcfb59e16966e866cb858539267e0
astha1410 <astha1410@gmail.com>
2025-05-27 23:23:35 +0530

    27th May 2025

---
+++ src/ex_01_Java_Basics/Lab002_Variables.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Variables {
+    public static void main(String[] args) {
+        int a = 65;
+        int age = 56;
+        age = age + 25;
+        System.out.println("age");
+        System.out.println(a);
+         a = 10;
+         age = 15;
+        System.out.println(a);
+        System.out.println(age);
+        int _ =10; // valid but won't run
+        int $ = 100;
+        System.out.println($);
+        int this_is_a_very_long_name = 200;
+        System.out.println(this_is_a_very_long_name);
+
+    }
+}
