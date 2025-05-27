bf8f3c9a4c2c20b5add55a9f9abd7465534c24c2
astha1410 <astha1410@gmail.com>
2025-05-27 20:05:31 +0530

    27th May 2025

---
+++ src/ex_01_Java_Basics/Lab001.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("2+2"); // treated as a string
+        System.out.println(3/2);// Print ln Always returns an integer
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.print(8/2);
+        System.out.println("hi");
+        System.out.println(10/0); // exception , not defined
+    }
+}
